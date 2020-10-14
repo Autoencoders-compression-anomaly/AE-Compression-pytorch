added linear mapping interval normalization, fixed HTCondor issues with pip and fastai

plotting/correlationPlots.py
@@ -79,16 +79,12 @@ def evaluateNetwork():
 
 def correlationPlots():
     makeCorner = False #Make corner plots
-    useNetwork = True #If using a network to make predictions
+    useNetwork = False #If using a network to make predictions
     curr_save_folder = '/eos/user/s/sarobert/TLAcorrelationPlots/'
+    pathToData = '/afs/cern.ch/work/s/sarobert/autoencoders/processedData/'
 
      # Figures setup
     plt.close('all')
-    unit_list = ['[GeV]', '[rad]', '[rad]', '[GeV]']
-    variable_list = [r'$p_T$', r'$\eta$', r'$\phi$', r'$E$']
-    line_style = ['--', '-']
-    colors = ['red', 'c']
-    markers = ['*', 's']
 
     # Histograms
     if useNetwork:
@@ -116,8 +112,12 @@ def correlationPlots():
 #        pred = pred.to_numpy()
 
     else:
-        data = pd.read_pickle(BIN + 'process_data/TLAJets.pkl')
+        data = pd.read_pickle(pathToData + 'TLAJets.pkl')
         columns = data.columns
+
+    data = filter_jets(data)
+    data, p = custom_normalization(data, data)
+    del p
 
     alph = 0.8
     n_bins = 80
@@ -126,7 +126,7 @@ def correlationPlots():
 
     plt.close('all')
     # Compute correlations
-    corr = res_df.corr()
+    corr = data.corr()
     print (corr)
     # Generate a mask for the upper triangle
     mask = np.zeros_like(corr, dtype=np.bool)
@@ -142,7 +142,7 @@ def correlationPlots():
                 square=True, linewidths=.5, cbar_kws={"shrink": .5})
     plt.subplots_adjust(left=.23, bottom=.30, top=.99, right=.99)
 
-    fig_name = 'TLAresidualsCorrelations.png'
+    fig_name = 'TLAnormCorrelations.png'
     plt.savefig(curr_save_folder + fig_name)
     
     if makeCorner:

plotting/thresholdMigration.py
@@ -19,16 +19,14 @@ def passClean(row): #All Cleaning Criteria
     else:
         return True
 
-def passVar(row):
-    Cut = .8 * 65535
-    var = 'AverageLArQF'
+def passVar(row, Cut, var):
     if np.abs(row[var]) < Cut:
         return False
     else:
         return True
 
 def thresholdMigration(data_str, pred_str):
-    curr_save_folder = '/eos/user/s/sarobert/TLAcorrelationPlots/'
+    curr_save_folder = '/eos/user/s/sarobert/thresholds2/'
     save = True
     plt.close('all')
 
@@ -40,29 +38,42 @@ def thresholdMigration(data_str, pred_str):
     
     dataPF = []
     predPF = []
+
+    var = 'EMFrac' #Which variable to be studied
+    cut = .05 #The cut value
+
     for index, row in data.iterrows():
-        passed = int(passClean(row))
-        #passed = int(passVar(row))
+        #passed = int(passClean(row))
+        passed = int(passVar(row, cut, var))
         dataPF.append(passed)
     
     for index, row in pred.iterrows():
-        passed = int(passClean(row))
-        #passed = int(passVar(row))
+        #passed = int(passClean(row))
+        passed = int(passVar(row, cut, var))
         predPF.append(passed)
-
-    counts, xedges, yedge, plot = plt.hist2d(dataPF, predPF, bins=2, range=[[0,1],[0,1]], cmap=plt.cm.jet, norm=mpl.colors.LogNorm())
     
+    fig = plt.figure()
+    ax = fig.add_axes([.1, .1, .8, .8])
+    labels = [var + ' < ' + str(cut), var + ' >' + str(cut)] #Doesn't work for badJet threshold
+    ax.set_xticks([.25, .75])
+    ax.set_yticks([.25, .75])
+    ax.set_xticklabels(labels)
+    ax.set_yticklabels(labels)
+    plt.rcParams.update({'figure.autolayout': True})
+
+    counts, xedges, yedge, plot = plt.hist2d(dataPF, predPF, bins=2, range=[[0, 1], [0, 1]], cmap=plt.cm.jet, norm=mpl.colors.LogNorm(), data=None)
     plt.colorbar()
-    plt.title('Reconstruction Migration around Avg LAr QF = .8')
-    plt.xlabel('Input (1: AvgLArQF > .8)')
+    plt.title('Reconstruction Migration around ' + var + ' = ' + str(cut))
+    plt.xlabel('Input')
     plt.ylabel('AE Reconstruction')
+
     for i, row in enumerate(counts):
         for j, binCount in enumerate(counts[i]):
             plt.text(float(i)/2 + 1/4, float(j)/2 + 1/4, str(counts[i][j]), color='lightgray', horizontalalignment='center', verticalalignment='center')
     if save:
-        plt.savefig(curr_save_folder + 'threshold_avgLArQF.png')
+        plt.savefig(curr_save_folder + var + str(cut) + '.png')
 
 pathToData = '/afs/cern.ch/work/s/sarobert/autoencoders/processedData/'
-data = pathToData + 'TLAJets_test_filter.pkl'
-pred = pathToData + 'pred_TLAJets_testing.pkl'
+data = pathToData + 'TLAJets_testing2_minFilter.pkl'
+pred = pathToData + 'pred_TLAJets_testing2_minFilter.pkl'
 thresholdMigration(data, pred)

plotting/varDistHists.py
@@ -11,10 +11,10 @@ def varDistHists():
     #curr_save_folder = ''
 
     #Which plots to make
-    makeRaw = True
+    makeRaw = False
     makeFilter = False
     makeMin = False
-    makeNorm = False
+    makeNorm = True
     makeMinNorm = False
 
     #Set up figures
@@ -55,8 +55,19 @@ def varDistHists():
         minNormTrain, minNormTest = custom_normalization(minTrain, minTest)
         dataMinNorm = pd.concat([minNormTrain, minNormTest]).to_numpy()
 
+    limitsDict = { #Cut thresholds for filtering scheme
+            'EMFrac' : [-5, 5],
+            'OotFracClusters5' : [-.1],
+            'OotFracClusters10' : [-.1],
+            'Width' : [-5, 5],
+            'WidthPhi' : [-5, 5],
+            'Timing' : [-125, 125],
+            'LArQuality' : [4],
+            'HECQuality' : [-2.5, 2.5],
+            'NegativeE' : [-300000]
+        }
     
-    #RawDistributions I apologize for the spaghetti
+    #RawDistributions 
     if makeRaw:
         for kk in np.arange(27):
             plt.figure()
@@ -98,6 +109,14 @@ def varDistHists():
             #if labels[kk] == 'LArQuality':
             #    plt.axvline(x=.8, color='red')
             #    plt.arrow(.8, 100000, dx = .15, dy = 0, width = .1, color='red')
+            try:
+                try:
+                    plt.axvline(x=limitsDict[labels[kk]][0], color = 'black')
+                    plt.axvline(x=limitsDict[labels[kk]][1], color = 'black')
+                except:
+                    plt.axvline(x=limitsDict[labels[kk]][0], color = 'black')
+            except:
+                pass
             plt.xlabel(labels[kk])
             plt.ylabel('Number of events')
             plt.yscale('log')
@@ -105,19 +124,7 @@ def varDistHists():
             plt.savefig(curr_save_folder + fig_name)
             plt.close('all')
 
-    if makeFilter:
-        limitsDict = {
-            'EMFrac' : [-5, 5],
-            'OotFracClusters5' : [-.1], 
-            'OotFracClusters10' : [-.1],
-            'Width' : [-5, 5],
-            'WidthPhi' : [-5, 5],
-            'Timing' : [-125, 125],
-            'LArQuality' : [4],
-            'HECQuality' : [-2.5, 2.5],
-            'NegativeE' : [-300, 300]
-        }
-    
+    if makeFilter:    
         #Distributions after filtering
         for kk in np.arange(27):
             plt.figure()
@@ -183,10 +190,10 @@ def varDistHists():
             n_hist_data, bin_edges, _ = plt.hist(dataNorm[:, kk], color=colors[1], label='Input', alpha=1, bins=n_bins)
             try:
                 try:
-                    plt.axvline(x=limitsDict[labels[kk]][0], color = 'black')
-                    plt.axvline(x=limitsDict[labels[kk]][1], color = 'black')
+                    plt.axvline(x=normLimitsDict[labels[kk]][0], color = 'black')
+                    plt.axvline(x=normLimitsDict[labels[kk]][1], color = 'black')
                 except:
-                    plt.axvline(x=limitsDict[labels[kk]][0], color = 'black')
+                    plt.axvline(x=normLimitsDict[labels[kk]][0], color = 'black')
             except:
                 pass
             plt.suptitle(labels[kk])