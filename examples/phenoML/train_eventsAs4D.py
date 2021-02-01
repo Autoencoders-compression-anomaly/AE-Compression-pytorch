fce08198b7693e0fe796160ba242b5bf5347b05d
Marija Vaskeviciute <2267670v@student.gla.ac.uk>
2021-02-01 12:54:05 +0000

    add training with large data

--- examples/phenoML/train_eventsAs4D.py
+++ examples/phenoML/train_eventsAs4D.py
@@ -6,17 +6,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
 import time
+import h5py
 import torch
 import torch.nn as nn
 import torch.utils.data
+import torch.utils.data as dt
 
-from torch.utils.data import TensorDataset, DataLoader
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from fastai import learner
 from fastai.data import core
 from fastai.metrics import mse
 from fastai.callback import schedule
+from scipy import stats
 from pathlib import Path
 
 # Need this to be able to access HEPAutoencoder library
@@ -62,6 +64,42 @@ class AE_3D_200_LeakyReLU(nn.Module):
     # Function describes autoencoder structure
     def describe(self):
         return 'in-200-200-20-3-20-200-200-out'
+
+class Dataset(dt.Dataset):
+    def __init__(self, fpath, transform=None):
+        super(Dataset, self).__init__()
+        f = h5py.File(fpath)
+        self.data = f.get('data')
+        self.target = f.get('data')
+        self.transform = transform
+
+    def __getitem__(self, index):
+        sample = (torch.from_numpy(self.data[index,:]).float(), \
+                  torch.from_numpy(self.target[index,:]).float())
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+
+    def __len__(self):
+        return self.data.shape[0]
+
+class Normalize(object):
+    def __init__(self, divisors):
+        assert isinstance(divisors, (list, tuple))
+        assert len(divisors) == 4
+        self.divisors = divisors
+
+    def __call__(self, sample):
+        for s in sample:
+            for i in range(len(s)):
+                if i in [0, 1]:
+                    s[i] = s[i] / self.divisors[i]
+                    s[i] = np.log10(s[i])
+                elif i == 2:
+                    s[i] = s[i] / self.divisors[i]
+                elif i == 3:
+                    s[i] = s[i] / self.divisors[i]
+        return sample
 
 # Function for parsing command line arguments
 # Returns: composite Object containing command line argument
@@ -130,6 +168,44 @@ def plot(data_in, data_out, col_names):
         plt.savefig('plts/comparison_{}.png'.format(str(col_names[col])))
         plt.close()
 
+def plot_residuals(data_in, data_out, col_names):
+    residual_strings = [r'$(m_{out} - m_{in}) / m_{in}$',
+                        r'$(p_{T,out} - p_{T,in}) / p_{T,in}$',
+                        r'$(\eta_{out} - \eta_{in}) / \eta_{in}$',
+                        r'$(\phi_{out} - \phi_{in}) / \phi_{in}$']
+    residuals = data_out - data_in
+    residuals = np.divide(residuals, data_in)
+    rang = (-0.1, 0.1)
+    for col in np.arange(4):
+        plt.figure()
+        std = np.nanstd(residuals[:, col])
+        std_err = np.nanstd(residuals[:, col], ddof=0) / np.sqrt(2 * len(residuals[:, col]))
+        mean = np.nanmean(residuals[:, col])
+        sem = stats.sem(residuals[:, col], nan_policy='omit')
+        plt.hist(residuals[:, col], alpha=0.8, bins=100, range=rang,
+                 label='Residuals \n Mean = {} $\pm$ {}\n $\sigma$ = {} $\pm$ {}'.format(mean, sem, std, std_err))
+        plt.title('Residuals of {}'.format(col_names[col]))
+        plt.xlabel(residual_strings[col])
+        plt.ylabel('Number')
+        plt.legend()
+        plt.savefig('plts/residual_{}.png'.format(col_names[col]))
+        plt.close()
+
+def plot_MSE(data_in, data_out):
+    mse = np.sum(np.power(data_in - data_out, 2), axis=1) / 4
+    plt.figure()
+    plt.plot(mse)
+    plt.title('Testing MSE Loss')
+    #plt.xlabel()
+    #plt.ylabel()
+    plt.savefig('plts/testing_mse_loss.png')
+    plt.close()
+
+def get_MSE(data_in, data_out):
+    l1 = np.sum(np.sum(np.power(data_in - data_out, 2), axis=1) / 4) / data_in.size
+    l2 = np.sum(np.power(data_in - data_out, 2)) / (data_in.size)
+    return l1, l2
+
 # Function to read data file locations from file
 # Arguments:
 #     string containing path to the file to be read
@@ -147,18 +223,19 @@ def main():
         train_path, test_path = read_dataname_file(args.dfile[0])
     # Check that training and testing datasets are formatted correctly
     train_filename, train_extension = os.path.splitext(train_path)
-    if (train_extension != '.pkl' or os.path.splitext(test_path)[1] != '.pkl'):
-        print('Invalid file type: Training and testing datasets must be in pickle (.pkl) format.')
-        return 1
+    _, test_extension = os.path.splitext(test_path)
+    if (train_extension not in ['.pkl', '.h5'] or test_extension not in ['.pkl', '.h5']):
+        print('Invalid file type: Training and testing datasets must be either in pickle (.pkl) or in HDF5 (.h5) format.')
+        return
     else:
         # Define files where training set split into training and validation will be stored
         train_split_path = train_filename + '_train' + train_extension
         valid_split_path = train_filename + '_valid' + train_extension
 
     # Define variables
-    bs = 8192
+    bs = 32 #8192
     loss_func = nn.MSELoss()
-    one_epochs = 30
+    one_epochs = 5
     wd = 1e-2
     recorder = learner.Recorder()
 
@@ -167,37 +244,66 @@ def main():
         fastai.torch_core.defaults.device = 'cuda'
         print('Using GPU for training')
 
-    # Get data from pkl file
-    data = pd.read_pickle(train_path)
-    test = pd.read_pickle(test_path)
+    # Get training data from a file
+    if train_extension == '.pkl':
+        data = pd.read_pickle(train_path)
+        print('Events in Training Set: {}'.format(data.shape[0]))
+    else:
+        f = h5py.File(train_path, 'r')
+        data = f['data'][...]
+        print('Events in Training Set: {}'.format(data.shape[0]))
+
+    # Get testing data from a file
+    if test_extension == '.pkl':
+        test = pd.read_pickle(test_path)
+        print('Events in Testing Set: {}'.format(test.shape[0]))
+    else:
+        f = h5py.File(test_path, 'r')
+        test = f['data'][...]
+        print('Events in Testing Set: {}'.format(test.shape[0]))
 
     # Split into training and validation datasets
     train, valid = train_test_split(data, test_size=0.2, random_state=41)
-    # Store split training and validation sets (change to point to your location)
-    train.to_pickle(train_split_path)
-    valid.to_pickle(valid_split_path)
-    # Read the datasets into pandas DataFrame
-    train = pd.read_pickle(train_split_path)
-    valid = pd.read_pickle(valid_split_path)
+
+    if train_extension == '.pkl':
+        # Store split training and validation sets
+        train.to_pickle(train_split_path)
+        valid.to_pickle(valid_split_path)
+        # Read the training dataset into pandas DataFrame
+        train = pd.read_pickle(train_split_path)
+        valid = pd.read_pickle(valid_split_path)
+    else:
+        with h5py.File(train_split_path, 'w') as f:
+            f.create_dataset('data', data=train)
+        with h5py.File(valid_split_path, 'w') as f:
+            f.create_dataset('data', data=valid)
 
     if (args.norm == 'custom'):
         # Custom normalise training and testing datasets
-        train = custom_normalise(train)
-        valid = custom_normalise(valid)
-        test = custom_normalise(test)
+        if train_extension == '.pkl':
+            train = custom_normalise(train)
+            valid = custom_normalise(valid)
+            test = custom_normalise(test)
+        else:
+            norm = Normalize([1000.0, 1000.0, 5.0, 3.0])
     elif (args.norm == 'std'):
-       # Standard normalise training and testing datasets
-       train = std_normalise(train)
-       valid = std_normalise(valid)
-       test = std_normalise(test)
+        # Standard normalise training and testing datasets
+        if train_extension == '.pkl':
+            train = std_normalise(train)
+            valid = std_normalise(valid)
+            test = std_normalise(test)
 
-    # Create TensorDatasets
-    train_ds = TensorDataset(torch.tensor(train.values, dtype=torch.float), torch.tensor(train.values, dtype=torch.float))
-    valid_ds = TensorDataset(torch.tensor(valid.values, dtype=torch.float), torch.tensor(valid.values, dtype=torch.float))
+    if train_extension == '.pkl':
+        # Create Tensor
+        train_ds = dt.TensorDataset(torch.tensor(train.values, dtype=torch.float), torch.tensor(train.values, dtype=torch.float))
+        valid_ds = dt.TensorDataset(torch.tensor(valid.values, dtype=torch.float), torch.tensor(valid.values, dtype=torch.float))
+    else:
+        train_ds = Dataset(train_split_path, transform=norm)
+        valid_ds = Dataset(valid_split_path, transform=norm)
 
     # Create DataLoaders
-    train_dl = DataLoader(train_ds, batch_size=bs, shuffle=True)
-    valid_dl = DataLoader(valid_ds, batch_size=bs * 2)
+    train_dl = dt.DataLoader(train_ds, batch_size=bs, shuffle=True)
+    valid_dl = dt.DataLoader(valid_ds, batch_size=bs * 2)
 
     # Return DataBunch
     db = core.DataLoaders(train_dl, valid_dl)
@@ -224,10 +330,11 @@ def main():
 
     if loss_plot:
         learn.recorder.plot_loss().savefig('plts/training_loss.png')
+        plt.close('all')
 
     # Get final validation loss
     print('MSE on validation set is {}'.format(learn.validate()))
-    
+
     # Make predictions
     data = torch.tensor(test.values, dtype=torch.float)
     predictions = model(data)
@@ -236,6 +343,10 @@ def main():
 
     # Plot results
     plot(data, predictions, test.columns)
+    plot_residuals(data, predictions, test.columns)
+    plot_MSE(data, predictions)
+    mse = get_MSE(data, predictions)
+    print('MSE1: {}; MSE2: {}'.format(mse[0], mse[1]))
 
 if __name__=='__main__':
     main()
